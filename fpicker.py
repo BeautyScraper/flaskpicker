05f80b226eb4ce41c7d6dd6fcc975faf47b6863f
Your Name <you@example.com>
2021-11-04 10:06:11 +0530

    partial support for config

--- fpicker.py
+++ fpicker.py
@@ -5,24 +5,20 @@ import random
 import re
 
 # inputDir = r'D:\paradise\stuff\Images\walls'
-inputDir = r'C:\Heaven\YummyBaked\SuperWemon4\SuperWemon1'
 
-cdir =[
+cnffile = 'config'
+# inputDir = r'C:\Heaven\YummyBaked\SuperWemon4\SuperWemon1\club9'
 
-r'D:\paradise\stuff\Essence\Images\Considerable',
-r'D:\paradise\stuff\Essence\Images\BeautyExpress',
-r'D:\paradise\stuff\Essence\Images\CaughtInAct'
-
-]
+cdir = []
 
 
-inputDirP = Path(inputDir)
-allImages = [x for x in inputDirP.glob('*.jpg')]
 # allImages =  random.shuffle(allImages)
 app = Flask(__name__)
 
 @app.route('/')
 def main():
+   if len(allImages) <= 0:
+        return 'No images left in the directory'
    imgfp = imageRender()
    return render_template("template.html",name=imgfp.name)
    # return 'hello world'
@@ -45,12 +41,11 @@ def noteDownFilename():
     dstfp.unlink()
         
         
-    # print(request.args['category1'])
+    # print(request.args['category1']).
     return redirect(url_for('main'))
    
 
 def imageRender():
-    # imgfp = random.choice(allImages) 
     imgfp = allImages.pop()
     dstfp = Path(__file__).parent / 'static' / 'images' / imgfp.name
     dstfp.parent.mkdir(parents=True,exist_ok=True)
@@ -58,4 +53,12 @@ def imageRender():
     return imgfp
 
 if __name__ == '__main__':
+   with open(cnffile,'r+') as fp:
+       inputDir = fp.readline().strip()
+       for x in fp.readlines():
+            line = x.strip()
+            cdir.append(line)
+   inputDirP = Path(inputDir)
+   allImages = [x for x in inputDirP.glob('*.jpg')]
+        
    app.run(host="0.0.0.0")
